e5cd2f729d452d9af1fd16797fdad6b3b10c4e3c
老船长PZ.Jack <shenchang01@bilibili.com>
2025-07-07 16:02:29 +0800

    feat: 自定义风格

--- eslint.config.ts
+++ eslint.config.ts
@@ -13,13 +13,29 @@ export default antfu(
     formatters: true,
     pnpm: true,
     type: 'lib',
+    stylistic: {
+      indent: 2,
+      quotes: 'single',
+      semi: false,
+      jsx: true,
+    },
   },
   {
     ignores: [
       'fixtures',
       '_fixtures',
       '**/constants-generated.ts',
+      'node_modules',
+      '**/node_modules/**',
+      'dist',
+      '**/dist/**',
     ],
+    rules: {
+      'ts/no-explicit-any': 'off',
+      'no-console': 'off',
+      'brace-style': ['error', '1tbs'],
+      'style/brace-style': ['error', '1tbs'],
+    },
   },
   {
     files: ['src/**/*.ts'],
